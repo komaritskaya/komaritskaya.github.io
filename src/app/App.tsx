605ec545d7e8c6be1e6ea5d8351ee93e2218e5d6
Nadezhda Komaritskaya <nk@anggell.com>
2024-09-03 21:30:34 +0300

    HW1: add info about myself

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,7 +8,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Текст писать тут
+          Всем привет!👋
+          <br />
+          Меня зовут Надежда, я фронтенд-разработчик с опытом 7 лет
+          <br />
+          Работала с: HTML, CSS, JS, Typescript, Flow, SASS, LESS, AngularJS 1.x, Angular 2+, React, React Native,
+          Redux, Redux-Saga, Redux-Thunk, Redux Toolkit, Jest + Enzyme, Jasmine + Karma, Node.js
+          <br />
+          Цель на курс - заполнить пробелы и приятно провести время
+          <hr />
+          <a href="https://t.me/komaritskaya">Мой ТГ</a>
         </p>
       </header>
     </div>
